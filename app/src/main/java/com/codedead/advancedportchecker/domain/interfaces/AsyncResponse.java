7a36bec1c1c6cb2bd916730aece63b7deca15dcc
CodeDead <admin@codedead.com>
2025-03-02 19:13:53 +0100

    feat: replaced deprecated features, removed shitter link, refactoring

--- app/src/main/java/com/codedead/advancedportchecker/domain/interfaces/AsyncResponse.java
+++ app/src/main/java/com/codedead/advancedportchecker/domain/interfaces/AsyncResponse.java
@@ -3,9 +3,5 @@ package com.codedead.advancedportchecker.domain.interfaces;
 import com.codedead.advancedportchecker.domain.object.ScanProgress;
 
 public interface AsyncResponse {
-    void scanComplete();
-
-    void scanCancelled();
-
     void update(final ScanProgress scanProgress);
 }
